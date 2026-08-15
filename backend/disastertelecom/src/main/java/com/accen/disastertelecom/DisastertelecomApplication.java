aac1b2792df507919f76086d5fd51ba64e7efed1
st00pid_cloud <justinemaesecret@gmail.com>
2026-08-15 23:52:56 +0800

    Initialized backend and scaffold stuff

---
+++ backend/disastertelecom/src/main/java/com/accen/disastertelecom/DisastertelecomApplication.java
@@ -0,0 +1,13 @@
+package com.accen.disastertelecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisastertelecomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisastertelecomApplication.class, args);
+	}
+
+}
---
+++ backend/disastertelecom/src/test/java/com/accen/disastertelecom/DisastertelecomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accen.disastertelecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisastertelecomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
